e2ff7d51fe4a408056830b2d13ca714f59fe6056
shrawan-shakya <gdesignershrawan@gmail.com>
2026-03-02 20:13:24 +1100

    Revert redirect logic in next config to resolve 500 error on vercel

--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.shakyagallery.com',
-          },
-        ],
-        destination: 'https://shakyagallery.com/:path*',
-        permanent: true,
-      },
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'header',
-            key: 'x-forwarded-proto',
-            value: 'http',
-          },
-        ],
-        destination: 'https://shakyagallery.com/:path*',
-        permanent: true,
-      }
-    ];
-  },
   images: {
     remotePatterns: [
       {
